051927d74d5720c3d687715b8c1dc0c3935ce3e0
Rahmane OUSMANE <ousmanedev@gmail.com>
2017-08-10 22:42:58 +0800

    Add Google forms to menu

--- js/app.js
+++ js/app.js
@@ -1,9 +1,15 @@
-$(document).on('click', 'a', function(event){
-  if(event.target.id == 'accueil')
-    return;
-  event.preventDefault();
+// $(document).on('click', 'a:not(.model-trigger)', function(event){
+//   event.preventDefault();
+//
+//   $('html, body').animate({
+//     scrollTop: $( $.attr(this, 'href') ).offset().top
+//   }, 500);
+// });
 
-  $('html, body').animate({
-    scrollTop: $( $.attr(this, 'href') ).offset().top
-  }, 500);
-});
+// $('a:not(.model-trigger)').click(function(e) {
+//   event.preventDefault();
+//
+//   $('html, body').animate({
+//     scrollTop: $( $.attr(this, 'href') ).offset().top
+//   }, 500);
+// })
